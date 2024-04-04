065b6591de109a21a4398bb41a4090e6b506ff6e
rohitmalekar <rohitmalekar@gmail.com>
2024-04-04 18:59:56 +0530

    Update wrapped.py

--- wrapped.py
+++ wrapped.py
@@ -197,8 +197,15 @@ def main():
     lookup_df['Start Date'] = pd.to_datetime(lookup_df['Start Date']).dt.tz_localize(None)
     lookup_df['End Date'] = pd.to_datetime(lookup_df['End Date']).dt.tz_localize(None)
 
+    query_params = st.query_params('address')
 
-    address = tcol2.text_input('Enter your Ethereum address below to uncover your unique impact story (starting "0x"):', help='ENS not supported, please enter 42-character hexadecimal address starting with "0x"')
+    if len(query_params) == 1:
+        address = query_params[0]
+        tcol2.text_input('Enter your Ethereum address below to uncover your unique impact story (starting "0x"):', 
+                                   value = query_params[0],
+                                   help='ENS not supported, please enter 42-character hexadecimal address starting with "0x"')    
+    else:
+        address = tcol2.text_input('Enter your Ethereum address below to uncover your unique impact story (starting "0x"):', help='ENS not supported, please enter 42-character hexadecimal address starting with "0x"')
 
     if address and address != 'None':
         my_bar = tcol2.progress(0, text='Looking up! Please wait.')
