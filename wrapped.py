9509ea8ecb267817e136acb7cb55919d25f605a1
rohitmalekar <rohitmalekar@gmail.com>
2024-04-05 17:56:15 +0530

    Update wrapped.py

--- wrapped.py
+++ wrapped.py
@@ -224,6 +224,7 @@ def main():
         address = tcol2.text_input('Enter your Ethereum address below to uncover your unique impact story (starting "0x"):', 
                                          help='ENS not supported, please enter 42-character hexadecimal address starting with "0x"')
 
+    st.markdown('<a href="/?address=" target="_self">Reset</a>',unsafe_allow_html=True)
     if address and address != 'None':
         my_bar = tcol2.progress(0, text='Looking up! Please wait.')
         if not re.match(r'^(0x)?[0-9a-f]{40}$', address, flags=re.IGNORECASE):
