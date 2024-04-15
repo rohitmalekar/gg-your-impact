0d2af000b27b10ce75534c2be02ee4ba02022a84
rohitmalekar <rohitmalekar@gmail.com>
2024-04-15 16:42:58 +0530

    Update wrapped.py

--- wrapped.py
+++ wrapped.py
@@ -393,25 +393,6 @@ def main():
 
                         my_bar.empty()
 
-                        custom_url = f"http://16.171.237.112:8501/?address={address}"
-
-                        st.markdown("🌟 **Share Your Impact!** 🌍 Let the world know how you've contributed to the open-source ecosystem and beyond with Gitcoin Grants. Inspire others with your journey! 💫")
-                        # Insert the custom URL into the HTML string using string formatting
-                        html_content = f"""
-                            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" 
-                            data-text="Check my @gitcoin Grants Impact Dashboard" 
-                            data-url="{custom_url}"
-                            data-show-count="false"
-                            data-size="Large" 
-                            height=10
-                            Tweet
-                            </a>
-                            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                        """
-                        
-                        with tcol2:
-                            components.html(html_content)
-
                 else:
                     my_bar.empty()
                     tcol2.write("While there are contributions made from this address on Grants Stack, no contribution data found for Gitcoin Grants.")                    
